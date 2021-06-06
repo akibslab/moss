b7cf8a245d464c2e76a40dad5060cd9b0c379b2f
Akib Hossain <akib.hossain.iub@gmail.com>
2021-06-06 23:41:05 +0600

    First Commit: Home page done, Bio desktop version done

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	// Slicknav
+		$('.mobile-menu').slicknav({
+	        closeOnClick: true,
+	        label: '',
+	        appendTo:'.mobile-menu-wrpr',
+	    });
+
+	    $(".slicknav_btn").on("click",function(){
+	    	$(this).toggleClass("active");
+	    	$("body").toggleClass("overlay");
+	    	return false;
+	    });
+
+	    $(".header-menu li").has("ul").addClass("has-submenu");
+
+	    $( "#newsletterInput" ).focus(function() {
+		  	$("#social-subscribe-section").toggleClass("newsletterActive");
+		});
+
+		$("#newsletterInput").on('focus', function(){
+		   $("body").addClass("overlay-2");
+		}).on('blur', function(){
+		   $("body").removeClass("overlay-2");
+		});
+        
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
